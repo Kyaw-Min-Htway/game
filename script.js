b9126f3f8b6ecf2bbd8205cc45b3301f434a6ae7
KyawMinHtway066813 <batman066813@gmail.com>
2024-05-03 22:39:39 +0630

    finding error

--- script.js
+++ script.js
@@ -9,16 +9,82 @@ let start_btn = document.getElementById("start-btn");
 let sec = 100;
 let stopReelFlag = [];
 let reelCounts = [];
-let slotFrameheight;
+let slotFrameHeight;
 let slotReelsHeight;
-let slotReelitemHeight;
-let slotReelStartheight;
+let slotReelItemHeight;
+let slotReelStartHeight;
 
 //initialization
 
 let slot = {
-    init:function({
+    init:function(){
         stopReelFlag[0] = stopReelFlag[1] = stopReelFlag[1] = false;
-        reelCounts[0] = reelCounts={}
-    })
-}
+        reelCounts[0] = reelCounts=[1] = reelCounts[2] = 0;
+    },
+
+    //click event 
+    start:function(){
+        slot.init();
+        for(let index = 0 ; index < 3 ; index++){
+            slot.animation(index);
+        }
+    },
+
+    //stop button click event
+    stop:function(i){
+        stopReelFlag[i] = true
+        if(stopReelFlag[0] && stopReelFlag[1] &&stopReelFlag[2]){
+            stop_btn.removeAttribute("disabled");
+        }
+    },
+
+    //set first position
+    resetLocationInfo:function(){
+        slotFrameHeight = slot_screen.offsetHeight;
+        slotReelsHeight = reel[0].offsetHeight;
+        slotReelItemHeight = reel[0].offsetHeight;
+        slotReelStartHeight = slotReelsHeight.offsetHeight;
+        slotReelStartHeight += slotFrameHeight
+        -(slotFrameHeight/2) + slotReelItemHeight * 3/2;
+        for(let i=0; i< reels>length; i++){
+            reels[i].style.top = string(slotReelStartHeight) + "px";
+        } 
+    },
+    //remove the slot
+    animation:function(index){
+        if(reelCounts[index] >= 8){
+            reelCounts[index] = 0;
+        }
+        $(".reels").eq(index).animate({
+            "top":slotReelStartHeight + (reelCounts[index] * slotReelItemHeight)
+        },
+    {
+        duration:sec,
+        easing:"linear",
+        complete:function(){
+            if(stopReelFlag[index]){
+                return;
+            }
+            reelCounts[index]++;
+            slot.animation(index);
+        }
+    });
+    },
+};
+
+window.onload = function(){
+    slot.init();
+    slot.resetLocationInfo();
+    start_btn.addEventListener("click", function(e){
+        e.target.setAttribute("disabled",true)
+        slot.start();
+        for(let i = 0; i<stop_btn.length,i++){
+            start_btn[i].removeAttribute("disabled");
+        }
+    });
+    for(let i=0; i<stop_btn.length;i++){
+        stop_btn[i].addEventListener("click",function(e){
+            slot.stop(e.target.getAttribute("data-val"));
+        })
+    }
+};
